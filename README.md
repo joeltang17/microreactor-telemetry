f57a27637933bcad9ad7beebcd78fcade6068306
Joel <joeltang17@gmail.com>
2025-11-04 17:30:20 -0500

    basic code for simulator, not finihsed

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Delta Lake for analysis with Spark SQL.
 # Data source
 Source: UC Irvine Machine Learning Repository
 https://www.kaggle.com/datasets/rinichristy/combined-cycle-power-plant-data-set-uci-data?resource=download
+
+Features consist of hourly average ambient variables
+
+Temperature (T) in the range 1.81°C and 37.11°C,
+Ambient Pressure (AP) in the range 992.89-1033.30 milibar,
+Relative Humidity (RH) in the range 25.56% to 100.16%,
+Exhaust Vacuum (V) in teh range 25.36-81.56 cm Hg,
+Net hourly electrical energy output (EP) 420.26-495.76 MW
--- simulator/generator.py
+++ simulator/generator.py
@@ -1,4 +1,19 @@
 import pandas as pd
 import numpy as np
+import json
+import time
+
+# ['AT', 'V', 'AP', 'RH', 'PE']: Temperature, Exhaust Vacuum, Ambient Pressure, Relative Humidity, Electrical Energy Output
 
 df = pd.read_csv('simulator/dataset/Power Plant Data.csv')
+
+def generate_reactor_reading(dataframe):
+    reading = {}
+    for col in dataframe.columns:
+        mean, std = dataframe[col].mean(), dataframe[col].std()
+        reading[col] = np.random.normal(mean, std)
+    return reading
+
+while True: # TODO make timer and send it to flink
+    record = generate_reactor_reading(df)
+    
